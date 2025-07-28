5d9e666ae515bea509e80e9ca26ffc5b18fd5f55
gdserafini <gdserafini@outlook.com>
2025-07-28 20:57:01 -0300

    add ingestion service

--- src/ingestion/ingest_cnpj.py
+++ src/ingestion/ingest_cnpj.py
@@ -5,14 +5,16 @@ import io
 import os
 import sys
 import time
-import logging
+from src.utils.logging import logger
+import pandas as pd
 
 
 dft_year = int(os.getenv('DFT_YEAR'))
 dft_month = int(os.getenv('DFT_MONTH'))
 dft_month_str = os.getenv('DFT_MONTH_STR')
+base_dir_path = 'data'
 BASE_URL = 'https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj'
-base_dir_path = '../../data'
+DFT_CHUNK_SIZE = int(os.getenv('DFT_CHUNK_SIZE'))
 
 
 class CnpjEnum(Enum):
@@ -39,35 +41,49 @@ def request_data(url: str) -> bytes:
     return response.content
 
 
-def save_bytes_data(data: bytes, path: str) -> None:
+def save_bytes_data(data: bytes, path: str):
     with zipfile.ZipFile(io.BytesIO(data)) as zf:
         csv_name = zf.namelist()[0]
         with zf.open(csv_name) as csv_file:
-            content = csv_file.read()
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    with open(path, 'wb') as f:
-        f.write(content)
+            reader = pd.read_csv(
+                csv_file, 
+                sep=';', 
+                encoding='latin1', 
+                dtype=str, 
+                chunksize=DFT_CHUNK_SIZE
+            )
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            for i, chunk in enumerate(reader):
+                mode = 'w' if i == 0 else 'a'
+                header = i == 0
+                chunk.to_csv(
+                    path, 
+                    mode=mode, 
+                    header=header, 
+                    index=False
+                )
 
 
 total_start = time.time()
-
-tmp_count = 0
-
 for enum in CnpjEnum:
-    start = time.time()
-    url = build_url(enum, dft_year, dft_month)
-    data = request_data(url)
-    file_name = f'{enum.value}-{dft_year}-{dft_month:02d}.csv'
-    save_bytes_data(
-        data, 
-        f'{base_dir_path}/bronze/{dft_year}/{dft_month_str}/{file_name}'
-    )
-    end = time.time()
-    print(f'[INFO] - {file_name} successfully saved - Time: {(end - start):.2f} seconds.')
-    tmp_count += 1
-    if tmp_count == 2: break
+    try:
+        start = time.time()
+        url = build_url(enum, dft_year, dft_month)
+        data = request_data(url)
+        file_name = f'{enum.value}-{dft_year}-{dft_month:02d}.csv'
+        save_bytes_data(
+            data, 
+            f'{base_dir_path}/bronze/{dft_year}/{dft_month_str}/{file_name}'
+        )
+        end = time.time()
+        logger.info(
+            f'{file_name} successfully saved - Time: {(end - start):.2f} seconds.'
+        )
+    except Exception as e:
+        logger.error(e)
 
 total_end = time.time()
-
-print('[INFO] - Ingestion successfully completed')
-print(f'[INFO] - CNPJ data ingestion total time: {(total_end - total_start):.2f} seconds.')
+logger.info('Ingestion successfully completed')
+logger.info(
+    f'CNPJ data ingestion total time: {(total_end - total_start):.2f} seconds.'
+)
